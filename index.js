5c5615dbc24aa40695cf59adf08211ee3e497cdc
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-10-19 17:12:31 +0600

    create delete protion when user click delete button product will delete

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors=require('cors')
 require('dotenv').config()
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors())
@@ -42,6 +42,12 @@ async function run() {
       const newProduct=req.body
       console.log(newProduct)
       const result =await productCollection.insertOne(newProduct)
+      res.send(result)
+    })
+    app.delete('/products/:id',async(req,res)=>{
+      const id =req.params.id;
+      const query ={_id: new ObjectId(id)}
+      const result=await productCollection.deleteOne(query)
       res.send(result)
     })
     // Send a ping to confirm a successful connection
